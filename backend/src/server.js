083328338ce59d8e7f6831396ee7a86e10384f25
shivani.rajput <shivani.rajput@officebeacon.com>
2025-09-12 17:03:26 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,15 +4,25 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.js";
 import messagesRoutes from "./routes/messages.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
+
 const PORT = process.env.PORT || 5000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messagesRoutes);
 
+//make ready for deployment
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log("Server is running on port:- " + PORT);
