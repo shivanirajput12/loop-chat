5755103375b397aca2e0f1fbad3a8860ce8f503a
shivani.rajput <shivani.rajput@officebeacon.com>
2025-09-15 14:41:44 +0530

    Remove environment variable checks for MONGO_URI and JWT_SECRET in database connection and token generation

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -5,7 +5,6 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    if(!MONGO_URI) throw new Error("MONGO_URI is not defined in environment variables");
    const conn =  await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected successfully:", conn.connection.host);
   } catch (error) {
--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,20 +1,19 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (userId, res) => {
-  const { JWT_SECRET } = process.env;
-  if (!JWT_SECRET)
-    throw new Error("JWT_SECRET is not defined in environment variables");
+  
+ const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+  expiresIn: "10d",
+ });
 
-  const token = jwt.sign({ userId }, JWT_SECRET, {
-    expiresIn: "10d",
-  });
+ res.cookie("jwt", token, {
+  httpOnly: true,  ///prevent client-side JS from reading the cookie (cross-site scripting attacks)
+  secure: process.env.NODE_ENV === "development" ? false : true, //only send cookie over HTTPS
+  sameSite: "strict",
+  maxAge: 10 * 24 * 60 * 60 * 1000, // 10 days
+ });
 
-  res.cookie("jwt", token, {
-    httpOnly: true, ///prevent client-side JS from reading the cookie (cross-site scripting attacks)
-    secure: process.env.NODE_ENV === "development" ? false : true, //only send cookie over HTTPS
-    sameSite: "strict",
-    maxAge: 10 * 24 * 60 * 60 * 1000, // 10 days
-  });
-
-  return token;
+ return token;
 };
+
+
